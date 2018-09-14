4357950a9823a5a116f498d5edd3783afedb1518
Allan de Miranda <allandemiranda@gmail.com>
2018-09-14 11:14:27 -0300

    correções no puzzles

--- jogo/include/manipulation_puzzles.h
+++ jogo/include/manipulation_puzzles.h
@@ -23,8 +23,8 @@
  * @param upper_rand Define parameter to upper randon
  * @param size Side size from game board
  */
-void game_board_response(std::vector < std::vector <char> > board, int const upper_rand, int const size){
-    std::srand(std::time(nullptr)); // Use current time as seed for random generator
+void game_board_response(std::vector <char> board, int const upper_rand, int const size){
+    //std::srand(std::time(nullptr)); // Use current time as seed for random generator
     int random_number = std::rand()/((RAND_MAX + 1u)/(upper_rand - 1)); /* < Random number to select the puzzles */
     // Add the board to the vector
     /**
@@ -42,14 +42,36 @@ void game_board_response(std::vector < std::vector <char> > board, int const upp
 	puzzles.close(); // Close de doc puzzçes.txt
     // Swaping to board vector
     int element = random_number * (size * size); /* < Position of the element "temp" to be copied */
-    for(int i(0); i<size; ++i){
-        for(int j(0); j<size; ++j){
-            board[i].push_back(temp[element]);
-            ++element;
-        }
+    for(int i(0); i<(size * size); ++i){
+            board.push_back(temp[element + i]);
     }
 }
 
-void generator_board_user(std::vector < std::vector <char> > board, int const size, int const dificult){
-    
+void generator_board_user(std::vector <char> board_final, std::vector < std::vector <char> > board_user, int const size, int const dificult){
+    // Preparing pixel that will be visible
+    int pixel_open[dificult];
+    for(int i(0); i<dificult; ++i){
+        //std::srand(std::time(nullptr)); // Use current time as seed for random generator
+        int random_number = std::rand()/((RAND_MAX + 1u)/((size * size) - 1)); /* < Random number to select the pixel that will be visible */
+        pixel_open[i] = random_number;
+    }
+    // Preparing the pixel puzzles User
+    int pixel(0);
+    for(int i(0); i<size; ++i){
+        for(int j(0); j<size; ++j){
+            bool pixel_to_open = false; // if the generated pixel is the need opened
+            for(int k(0); k<dificult; ++k){
+                if(pixel_open[k] == pixel){
+                    pixel_to_open = true;
+                    break;
+                }
+            }
+            if(pixel_to_open){
+                board_user[i].push_back(board_final[pixel]);
+            } else {
+                board_user[i].push_back('0');
+            }
+            ++pixel;
+        }
+    }
 }
