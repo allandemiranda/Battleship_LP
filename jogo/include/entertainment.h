fbd80c84e083358bd6943477cedebc706a665c1f
Allan de Miranda <allandemiranda@gmail.com>
2018-09-14 09:21:28 -0300

    funçao welcome finalizada

---
+++ jogo/include/entertainment.h
@@ -0,0 +1,37 @@
+/**
+ * @brief User entertainment functions
+ * 
+ * @file entertainment.h
+ * @author allandemiranda
+ * @date 2018-09-14
+ */
+
+#include <iostream> // std::cout, std::endl
+
+/**
+ * @brief Welcome function and initial interaction
+ * 
+ * @param upper_limit Upper limit to the game board
+ * @param low_limit Low limit to the game board
+ * @return int Size of game board side
+ */
+int welcome(const int upper_limit, const int low_limit){
+    std::cout << "-|--- Welcome to Battleship ---|-" << std::endl;
+    int size(0); /* < Game board size */
+    // Checking the input value to "size"
+    while(true){
+        std::cout << "What game board size you want? (side)" << std::endl;
+        std::cin >> size;
+        if((size <= upper_limit) and (size >= low_limit)){
+            // The value is correct
+            break;
+        } else {
+            // The value is incorrect
+            std::cout << "Sorry!" << std::endl;
+            std::cout << "The low limit is " << low_limit;
+            std::cout << " and the upper limit is " << upper_limit << std::endl;
+            std::cout << "Please, enter a number that does not exceed the limits." << std::endl;
+        }
+    }
+    return size;
+}
---
+++ jogo/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @brief 
+ * 
+ * @file main.cpp
+ * @author allandemiranda JO5U3 
+ * @date 2018-09-14
+ */
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    // Receber tamanho do Puzzles
+    // Selecinar um Puzzles gerado
+    // Imrimir 5 posições abertas aleatoriamente
+    // Esperar entrada de linha x coluna
+    // Imprimir status dos barcos a cada interação
+    // Ao preencher todo campo retornar se acertou ou não
+
+    return 0;
+}
