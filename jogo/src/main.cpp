7bc8c5bbc21b8b013bb64f6b5d5e4b2a643071e1
Allan de Miranda <allandemiranda@gmail.com>
2018-09-14 12:11:48 -0300

    gráfico terminado e adicioando a main

--- jogo/src/main.cpp
+++ jogo/src/main.cpp
@@ -11,6 +11,10 @@
 #define LOW_LIMIT_SIDE 7 // Low limit to the game board side
 #define MAX_OF_PUZZLES_GENERATED 3 // Maximum of Puzzles generated per file
 #define DIFICULT_PIXELS 5 // How many pixels is to open
+#define QUANTITY_OF_SUBMARINES 4 // Submarines (one square in size)
+#define QUANTITY_OF_DESTROYERS 3 // Destroyers (two squares long)
+#define QUANTITY_OF_CRUISERS 2 // Cruisers (three squares long)
+#define QUANTITY_OF_BATTLESHIP 1 // Battleship (four squares long)
 
 // Containers library
 #include <vector> // std::vector container
@@ -37,6 +41,9 @@ int main(void)
     // The game Puzzles to the User
     std::vector < std::vector <char> > puzzles_user;
     generator_board_user(final_board, puzzles_user, size_board, DIFICULT_PIXELS);
+
+    // Print
+    dashboard_user_print(final_board, puzzles_user, QUANTITY_OF_SUBMARINES, QUANTITY_OF_DESTROYERS, QUANTITY_OF_CRUISERS, QUANTITY_OF_BATTLESHIP);
 
 
 
